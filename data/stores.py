342768bc0655fd7a5fb9740a23867b9d84a849ac
jganguita <godric14@gmail.com>
2024-10-28 18:23:55 +0100

    Add Cabanyal data

--- data/stores.py
+++ data/stores.py
@@ -2,4 +2,6 @@
 supermarkets = {
     "Aldi Brillante": {"lat": 37.904254757197386, "lon": -4.789535453730883},
     "Mercadona Arruzafa": {"lat": 37.9066249741828, "lon": -4.795757441201077},
+    "Mercadona Cabanyal": {"lat": 39.46671379528522, "lon": -0.3324399961848783},
 }
+
